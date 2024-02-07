de34cd86d7ef5ad9c59a1afc3844610c3b9b73a1
Soroush Mazloum <mazloomsoroush@gmail.com>
2024-02-07 16:30:11 +0330

    Pr9

---
+++ Practice9/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int number;
+    cout<<"Enter a number: ";
+    cin>>number;
+    int multiplication = 1;
+    int sum = 0;
+    while(number >= 1)
+    {
+        int digit = number % 10;
+        number % 10;
+        number /= 10;
+        sum += digit;
+        multiplication *= digit;
+    }
+    cout<<"Sum: "<<sum<<endl;
+    cout<<"Multiplication: "<<multiplication<<endl;
+}
